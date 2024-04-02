4ca9e70767cd36b8c710562975c4d6fb7c71297e
Illia Kornyk <krekerbrow@gmail.com>
2024-04-02 17:15:56 +0300

    refactor: use http.Error func

--- main.go
+++ main.go
@@ -23,8 +23,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
 
