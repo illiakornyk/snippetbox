4cb3b67230bcc2d2627ed50a29d09e2b05ca4b6b
Illia Kornyk <krekerbrow@gmail.com>
2024-04-11 13:33:22 +0300

    feat: insert method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -47,11 +47,21 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != "POST" {
+	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	w.Write([]byte("Create a new snippet"))
+	title := "O snail"
+    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+    expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		        // app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
